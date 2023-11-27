04613b944b028dde34a069a6af44894bc0bd1b7e
Simon Fogliato <simonfogliato@gmail.com>
2023-11-27 12:40:03 -0800

    Moved images into section.

--- app/page.tsx
+++ app/page.tsx
@@ -37,8 +37,11 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative"
           src="/arch1.png"
           alt="Arch 1"
           width={180}
@@ -46,7 +49,7 @@ export default function Home() {
           priority
         />
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative"
           src="/arch2.png"
           alt="Arch 2"
           width={180}
@@ -54,7 +57,7 @@ export default function Home() {
           priority
         />
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative"
           src="/arch3.png"
           alt="Arch 3"
           width={180}
@@ -62,7 +65,7 @@ export default function Home() {
           priority
         />
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative"
           src="/arch4.png"
           alt="Arch 4"
           width={180}
