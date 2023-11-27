c73c530bae8bedfc2b5fb19736f5dd27eaa68e56
Simon Fogliato <simonfogliato@gmail.com>
2023-11-27 12:06:32 -0800

    Added arch images.

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,38 @@ export default function Home() {
           height={37}
           priority
         />
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/arch1.png"
+          alt="Arch 1"
+          width={180}
+          height={37}
+          priority
+        />
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/arch2.png"
+          alt="Arch 2"
+          width={180}
+          height={37}
+          priority
+        />
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/arch3.png"
+          alt="Arch 3"
+          width={180}
+          height={37}
+          priority
+        />
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/arch4.png"
+          alt="Arch 4"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
